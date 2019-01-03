dcd64809ffe37b099bfaee3621f3dd3c86983710
Guillaume Lebeau <guillaume.lebeau@vivaticket.com>
2019-01-03 17:40:11 +0100

    Add Google authentication provider and use user secrets

--- image-gallery/src/Marvin.IDP/Startup.cs
+++ image-gallery/src/Marvin.IDP/Startup.cs
@@ -1,21 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+
+using IdentityServer4;
+
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Marvin.IDP
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddAuthentication()
+                .AddGoogle(
+                    "Google",
+                    o =>
+                    {
+                        // Use Secret Manager to define Google ClientId and ClientSecret
+                        // https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-2.2&tabs=windows
+                        // > dotnet user-secrets set "Authentication:Google:ClientId" "......"
+                        // > dotnet user-secrets set "Authentication:Google:ClientSecret" "......"
+                        // Check secrets with: > dotnet user-secrets list
+                        o.SignInScheme =
+                            IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                        o.ClientId = Configuration["Authentication:Google:ClientId"];
+                        o.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+                        
+                        o.UserInformationEndpoint = "https://openidconnect.googleapis.com/v1/userinfo";
+                        o.ClaimActions.Clear();
+                        o.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+                        o.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                        o.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_Name");
+                        o.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_Name");
+                        o.ClaimActions.MapJsonKey("urn:google:profile", "profile");
+                        o.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+                        o.ClaimActions.MapJsonKey("urn:google:image", "picture");
+                    });
             
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
