efb37de6b2a4907be957d4e568a7e16a41218eef
Guillaume Lebeau <guillaume.lebeau@vivaticket.com>
2019-01-04 10:33:36 +0100

    Add Microsoft authentication provider

--- image-gallery/src/Marvin.IDP/Startup.cs
+++ image-gallery/src/Marvin.IDP/Startup.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
 
 using IdentityServer4;
 
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,16 +29,22 @@ namespace Marvin.IDP
 
             services.AddAuthentication()
                 .AddGoogle(
-                    "Google",
+                    GoogleDefaults.AuthenticationScheme,
                     o =>
                     {
+                        o.SignInScheme =
+                            IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                        
+                        // Create the app in Google API Console
+                        // https://docs.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-2.2
+                        //
                         // Use Secret Manager to define Google ClientId and ClientSecret
                         // https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-2.2&tabs=windows
                         // > dotnet user-secrets set "Authentication:Google:ClientId" "......"
                         // > dotnet user-secrets set "Authentication:Google:ClientSecret" "......"
-                        // Check secrets with: > dotnet user-secrets list
-                        o.SignInScheme =
-                            IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                        //
+                        // Check secrets with:
+                        // > dotnet user-secrets list
                         o.ClientId = Configuration["Authentication:Google:ClientId"];
                         o.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
                         
@@ -54,6 +57,25 @@ namespace Marvin.IDP
                         o.ClaimActions.MapJsonKey("urn:google:profile", "profile");
                         o.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
                         o.ClaimActions.MapJsonKey("urn:google:image", "picture");
+                    })
+                .AddMicrosoftAccount(MicrosoftAccountDefaults.AuthenticationScheme,
+                    options =>
+                    {
+                        options.SignInScheme = 
+                            IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                        
+                        // Create the app in Microsoft Developer Portal
+                        // https://docs.microsoft.com/en-us/aspnet/core/security/authentication/social/microsoft-logins?view=aspnetcore-2.2
+                        //
+                        // Use Secret Manager to define Microsoft ApplicationId and Password
+                        // https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-2.2&tabs=windows
+                        // > dotnet user-secrets set "Authentication:Microsoft:ApplicationId" "......"
+                        // > dotnet user-secrets set "Authentication:Microsoft:Password" "......"
+                        //
+                        // Check secrets with:
+                        // > dotnet user-secrets list
+                        options.ClientId = Configuration["Authentication:Microsoft:ApplicationId"];
+                        options.ClientSecret = Configuration["Authentication:Microsoft:Password"];
                     });
             
             services.AddIdentityServer()
